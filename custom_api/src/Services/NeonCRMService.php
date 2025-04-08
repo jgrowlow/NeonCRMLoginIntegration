6f8b6426ab7daa5078a9be619c583dfbdce13540
jgrowlow <128536296+jgrowlow@users.noreply.github.com>
2025-04-08 15:01:25 -0400

    Update NeonCRMService.php

--- custom_api/src/Services/NeonCRMService.php
+++ custom_api/src/Services/NeonCRMService.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace CustomApi\Services;
 
 use GuzzleHttp\Client;
@@ -16,7 +15,7 @@ class NeonCRMService
     public function __construct(LoggerInterface $logger)
     {
         $this->client = new Client();
-        $this->apiUrl = 'https://api.neoncrm.com/v2/accounts/{id}';
+        $this->apiUrl = 'https://api.neoncrm.com/v2/accounts/';
         $this->apiKey = getenv('NEONCRM_API_KEY');
         $this->logger = $logger;
     }
@@ -24,33 +23,31 @@ class NeonCRMService
     /**
      * Get the user's email from NeonCRM by user ID.
      */
-    public function getUserEmail($userId)
+    public function getUserEmail($accountId)
     {
         try {
-            $response = $this->client->request('GET', "{$this->apiUrl}{$userId}", [
+            // Make the API call to NeonCRM to get account details
+            $response = $this->client->request('GET', $this->apiUrl . $accountId, [
                 'headers' => [
                     'Authorization' => "Bearer {$this->apiKey}",
                     'Accept' => 'application/json',
                 ],
+                'timeout' => 10,  // Optional: Set a timeout to avoid hanging indefinitely
             ]);
-
+            
+            // Decode the response
             $data = json_decode($response->getBody(), true);
-
-            if (isset($data['individualAccount'])) {
-                return $data['individualAccount']['email1'] ?? 
-                       $data['individualAccount']['email2'] ?? 
-                       $data['individualAccount']['contact']['email'] ?? 
-                       null;
+            
+            // Check for the primary contact's email
+            if (isset($data['primaryContact']['email1'])) {
+                return $data['primaryContact']['email1'];
+            } elseif (isset($data['primaryContact']['email2'])) {
+                return $data['primaryContact']['email2'];
+            } elseif (isset($data['primaryContact']['email3'])) {
+                return $data['primaryContact']['email3'];
             }
-
-            $this->logger->warning("NeonCRM response missing 'individualAccount' for user {$userId}");
-            return null;
-        } catch (RequestException $e) {
-            $this->logger->error("NeonCRM API error for user {$userId}: " . $e->getMessage());
-            return null;
-        } catch (\Exception $e) {
-            $this->logger->error("Unexpected error for user {$userId}: " . $e->getMessage());
+            
+            // If no email found, log and return null
+            $this->logger->warning("No valid email found for account {$accountId}");
             return null;
         }
-    }
-}
