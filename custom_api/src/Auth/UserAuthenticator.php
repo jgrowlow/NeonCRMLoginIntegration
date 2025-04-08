a49141f94de04bdd41d90e8de4dbf1bc4e518fcf
jgrowlow <128536296+jgrowlow@users.noreply.github.com>
2025-04-08 15:06:14 -0400

    Update UserAuthenticator.php

--- custom_api/src/Auth/UserAuthenticator.php
+++ custom_api/src/Auth/UserAuthenticator.php
@@ -1,33 +1,43 @@
 <?php
-
 namespace CustomApi\Auth;
 
 use Flarum\User\Authenticator;
 use Flarum\User\User;
 use Illuminate\Contracts\Encryption\Encrypter;
 use Psr\Log\LoggerInterface;
+use Flarum\Http\Redirector;
 
 class UserAuthenticator implements Authenticator
 {
     protected $logger;
+    protected $redirector;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, Redirector $redirector)
     {
         $this->logger = $logger;
+        $this->redirector = $redirector;
     }
 
-    public function authenticate($accountId)
+    /**
+     * Authenticate the user by their NeonCRM accountId.
+     *
+     * @param string $email
+     * @return User|null
+     */
+    public function authenticate($email)
     {
-        // Attempt to find the user in Flarum by the NeonCRM accountId
-        $user = User::where('neoncrm_account_id', $accountId)->first();
-
+        // Attempt to find the user by email in Flarum
+        $user = User::where('email', $email)->first();
+        
         if ($user) {
             // Successfully found the user, log them in
             return $user;
-        } else {
-            // No user found, log and return null
-            $this->logger->warning("User not found for accountId: {$accountId}");
-            return null;
         }
+
+        // Log if no user is found
+        $this->logger->warning("No user found for email: {$email}");
+        
+        // Redirect to the specified URL if no user is found
+        return $this->redirector->to('https://www.hopecommunitycenter.org/join');
     }
 }
